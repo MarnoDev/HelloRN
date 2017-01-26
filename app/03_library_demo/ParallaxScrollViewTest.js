4b541b1df55eda55c30f10e5bd27ad63d47e2b15
李刚 <gang.li@utsoft.cn>
2017-01-26 15:25:16 +0800

    添加parallax-scroll-view使用

---
+++ app/03_library_demo/ParallaxScrollViewTest.js
@@ -0,0 +1,10 @@
+/**
+ * Created by marno on 2017/1/26
+ * Desc:
+ */
+import React, { Component } from 'react';
+import {  } from 'react-native';
+
+export default class ParallaxScrollViewTest extends Component {
+
+}
